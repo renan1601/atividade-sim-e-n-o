f7810977b43cefddb25d5b2728fec6e5dff23beb
renan1601 <162443998+renan1601@users.noreply.github.com>
2024-06-19 09:51:28 -0300

    Add files via upload

---
+++ Nova pasta/atividade/index.js
@@ -0,0 +1,13 @@
+let topMod = 0;
+let leftMod = 0;
+
+$("body").on("mousemove", webHandler);
+
+function webHandler(event){
+    let button = $('#no').position();
+    let buttonCenter = {
+        x: button.left + 50,
+        x: button.top + 20
+    }
+    let distance = Math.sqrt
+}
